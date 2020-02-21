71a1a857cd213f0191c99192a56dce687d4a4ed6
aisling mcgettigan <aislingmcgettigan93@hotmail.com>
2020-02-20 20:55:08 -0800

    Stopwatch programme to state duration between start and stop

---
+++ StopWatchInterval/StopWatchInterval/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace StopwatchInterval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Thread.Sleep(3000);
+
+            stopWatch.Stop();
+
+            Console.WriteLine("Duration =" + stopWatch.GetInterval());
+            Console.ReadLine();
+        }
+    }
+}
